2ebe4c4817f71ec5e2df2c45d8464723ecca866f
Vadik365 <vadiksoldatenko@gmail.com>
2026-06-20 11:20:37 +0300

    add queries for filtering and annotating books and categories

--- books/views.py
+++ books/views.py
@@ -1,10 +1,4 @@
 from django.shortcuts import render
-from books.models import Book
 
 
 # Create your views here.
-def main_page(request):
-    result = Book.objects.all()
-    author = Book.objects.get(pk=1)
-    result = Book.objects.filter(author=author)
-    return ''
